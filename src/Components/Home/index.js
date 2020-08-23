9e743ee4282e36ba6b71611ab3b6a3a23ac82f30
Mrwan Zein <mrwanzein@outlook.com>
2020-08-23 16:02:49 -0400

    Created the base components for Tetris & misc

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Tetris from '../Tetris';
+
 const Homepage = () => {
     return(
-        <div>Home</div>
+        <Wrapper>
+            <Tetris />
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 80px);
+`;
 
 export default Homepage;

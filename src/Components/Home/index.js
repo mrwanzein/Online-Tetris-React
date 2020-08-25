9046b68ac4ab133c2846558a64d1fa9032f9d648
Mrwan Zein <mrwanzein@outlook.com>
2020-08-25 13:58:59 -0400

    Added hooks for player & stage, tetrominos is now rendered on stage

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,17 +5,10 @@ import Tetris from '../Tetris';
 
 const Homepage = () => {
     return(
-        <Wrapper>
+        <>
             <Tetris />
-        </Wrapper>
+        </>
     )
 }
-
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 80px);
-`;
 
 export default Homepage;
--- src/Components/Tetris/index.js
+++ src/Components/Tetris/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Stage from './Stage';
@@ -6,20 +6,65 @@ import Display from './Display';
 import StartButton from './StartButton'
 
 import { createStage } from './gameHelpers';
+import { usePlayer} from './hooks/usePlayer';
+import { useStage } from './hooks/useStage';
 
-const Tetris = ({ callback }) => {
+const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
+
+    console.log("re-render")
+    
+    const movePlayer = (direction) => {
+        updatePlayerPos({ x: direction, y: 0 });
+    }
+
+    const startGame = () => {
+        // Resets everything
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false });
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+    const move = ({ keyCode }) => {
+        if(!gameOver) {
+            if(keyCode === 37) {
+                movePlayer(-1);
+            } else if(keyCode === 39) {
+                movePlayer(1);
+            } else if(keyCode === 40) {
+                dropPlayer();
+            }
+        }
+    }
+
     return(
-        <TetrisStage>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                </div>
-                <StartButton />
-            </aside>
-        </TetrisStage>
+        <InputWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+            <TetrisStage>
+                <Stage stage={stage} />
+                <aside>
+                    {
+                        gameOver ? <span>Game Over!</span> : 
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    }
+                    <StartButton callback={startGame}/>
+                </aside>
+            </TetrisStage>
+        </InputWrapper>
     )
 }
 
@@ -28,6 +73,17 @@ const TetrisStage = styled.div`
     
     aside {
         margin-left: 20px;
+    }
+`;
+
+const InputWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 80px);
+    
+    &:focus {
+        outline: none;
     }
 `;
 

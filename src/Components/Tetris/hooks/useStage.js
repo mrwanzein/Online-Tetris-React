428f248386782277f61a860aeecfc176ab4169dc
Mrwan Zein <mrwanzein@outlook.com>
2020-08-26 13:40:15 -0400

    Added row clearance when filled

--- src/Components/Tetris/hooks/useStage.js
+++ src/Components/Tetris/hooks/useStage.js
@@ -3,8 +3,24 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        // Clear rows when filled
+        const sweepRows = newStage => {
+            return newStage.reduce((acc, row) => {
+                if(row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return acc;
+                }
+                acc.push(row);
+                return acc;
+            }, []);
+        }
+
         const updateStage = prevStage => {
             // First flush the stage
             const newStage = prevStage.map(row => {
@@ -26,6 +42,7 @@ export const useStage = (player, resetPlayer) => {
             // Check collision
             if(player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
 
             return newStage;
@@ -33,7 +50,7 @@ export const useStage = (player, resetPlayer) => {
 
         setStage(prev => updateStage(prev));
 
-    }, [player, resetPlayer]);
+    }, [player, resetPlayer, rowsCleared]);
 
     return [stage, setStage];
 }
